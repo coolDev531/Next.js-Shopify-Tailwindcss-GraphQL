bbaba56d9195badbe5babdb6a663d87cc2ccbc3a
HaradaYasuo <academic.dev0531@gmail.com>
2022-05-15 19:41:16 +0200

    Initial commit

--- _client/stores/posts-store.tsx
+++ _client/stores/posts-store.tsx
@@ -1,7 +1,6 @@
 import { makeStore } from "_client/stores/_make-store";
-import { Post } from ".prisma/client";
 
-const { Provider, useStore } = makeStore<Post[]>([], "Posts");
+const { Provider, useStore } = makeStore([], "Posts");
 
 export const usePostStore = useStore;
 export const PostsProvider = Provider;
