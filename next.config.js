d7a522c76e5f23bc1305a11f62200e5e931fd44d
HaradaYasuo <academic.dev0531@gmail.com>
2022-07-25 11:00:30 +0200

    Tab scroll container

--- next.config.js
+++ next.config.js
@@ -12,7 +12,6 @@ module.exports = withBundleAnalyzer({
   api: {
     responseLimit: "8mb",
   },
-  strictMode: true,
   images: {
     dangerouslyAllowSVG: true,
     deviceSizes: [460, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
