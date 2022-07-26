642f6ab7cccff57af4edc5fb995d8750e6d67e1a
HaradaYasuo <academic.dev0531@gmail.com>
2022-07-26 11:39:28 +0200

    Tab scroll container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,33 +16,33 @@ import "styles/utils.scss";
 import superjson from "superjson";
 
 export const SEO = {
-  title: "Felix Tellmann",
-  url: "https://flext.dev",
+  title: "Lunalemon - Web Development",
+  url: "https://lunalemon.dev",
   description:
-    "I'm a self-taught Full-stack Engineer, writer and entrepreneur living in Cape Town. I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. My goal is to always build products that provide real value to its users.",
-  siteName: "Flext.dev",
-  github: "https://github.com/FelixTellmann/flext.dev",
-  avatar: "https://avatars.githubusercontent.com/u/22034038",
-  author: "Felix Tellmann",
+    "International Shopify Web design studio - We create Shopify Ecommerce solutions - Empowering independent business owners everywhere.",
+  siteName: "Lunalemon.dev",
+  github: "https://github.com/luna-lemon",
+  avatar: "https://avatars.githubusercontent.com/u/64978215",
+  author: "Lunalemon.dev",
   twitter: {
-    handle: "@Tellmann",
-    site: "@FelixTellmann",
+    handle: "@lunalemon_dev",
+    site: "@lunalemon_dev",
     cardType: "summary_large_image",
   },
   openGraph: {
     type: "website",
     locale: "en_IE",
-    url: "https://flext.dev",
-    site_name: "Flext.dev",
-    title: "Felix Tellmann",
+    url: "https://lunalemon.dev",
+    site_name: "Lunalemon.dev",
+    title: "Lunalemon - Web Development",
     description:
-      "Lets make things better with quality code - Learn Web Development / API's / Automations / Serverless / Architecture / and more.",
+      "International Shopify Web design studio - We create Shopify Ecommerce solutions - Empowering independent business owners everywhere.",
     images: [
       {
-        url: "https://flext.dev/images/og-default.jpg",
-        alt: "Felix Tellmann - Full-stack Engineer - Entrepreneur",
-        width: 1200,
-        height: 630,
+        url: "https://lunalemon.dev/images/twitter.jpg",
+        alt: "Lunalemon - Web Development",
+        width: 1600,
+        height: 900,
       },
     ],
   },
