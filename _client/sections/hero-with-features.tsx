7229e7f52949a9dc757b1c23914c70c04559507b
HaradaYasuo <academic.dev0531@gmail.com>
2022-08-01 14:44:21 +0200

    Experimental changes ..

--- _client/sections/hero-with-features.tsx
+++ _client/sections/hero-with-features.tsx
@@ -4,7 +4,7 @@ import { Image } from "_client/image";
 import { Wrapper } from "_client/layout/wrapper";
 import { Link } from "_client/link";
 import { Richtext } from "_client/typography/richtext";
-
+import backgroundImage from "public/images/bg-gradient-templates.png";
 import { FC } from "react";
 import { HeroWithFeaturesSection } from ".shopify-cms/types/sections";
 
@@ -12,7 +12,7 @@ export const HeroWithFeatures: FC<HeroWithFeaturesSection> = ({ id, settings, bl
   return (
     <div className="relative overflow-hidden">
       <div className="absolute inset-0 h-full w-full border-b border-gray-200 bg-gray-50 dark:border-gray-700 dark:bg-dark-bg" />
-      <div className="absolute inset-y-0 hidden w-full min-w-[1360px] bg-[url('/images/bg-gradient-templates.png')] bg-[length:2000px_100%] bg-[position:calc(50%_+_220px)_-50px] bg-no-repeat lg:block" />
+      <div className="absolute inset-y-0 hidden w-full min-w-[1360px] bg-[url('/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbg-gradient-templates.0fc06eb1.png&w=1200&q=10')] bg-[length:2000px_100%] bg-[position:calc(50%_+_220px)_-50px] bg-no-repeat lg:block" />
       <Wrapper
         maxWidth="xl"
         spacing={settings.spacing}
