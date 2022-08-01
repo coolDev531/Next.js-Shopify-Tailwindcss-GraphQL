83d334b52c822dd1179e674c25e6f3d5c4b15f2d
HaradaYasuo <academic.dev0531@gmail.com>
2022-08-01 15:03:38 +0200

    Experimental changes ..

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ module.exports = withBundleAnalyzer({
   api: {
     responseLimit: "8mb",
   },
+  productionBrowserSourceMaps: true,
   images: {
     dangerouslyAllowSVG: true,
     deviceSizes: [460, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
