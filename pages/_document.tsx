241f5394714c5b9d0bda9b718c58dbe2e882e714
HaradaYasuo <academic.dev0531@gmail.com>
2022-08-04 10:50:08 +0200

    Experimental changes ..

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ import Script from "next/script";
 
 class Root extends Document {
   render(): JSX.Element {
+    // language=JavaScript
     return (
       <Html lang="en">
         <Head>
@@ -44,6 +45,40 @@ class Root extends Document {
             
           `}
           </Script>
+          <Script id="tawk-script" strategy="afterInteractive">
+            {`<!-- Meta Pixel Code -->
+              !function (f, b, e, v, n, t, s) {
+                if (f.fbq) return; 
+                n = f.fbq = function () {
+                  n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+                };
+                if (!f._fbq) f._fbq = n;
+                n.push = n;
+                n.loaded = !0;
+                n.version = '2.0';
+                n.queue = [];
+                t = b.createElement(e);
+                t.async = !0;
+                t.src = v;
+                s = b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t, s);
+              }(window, document, 'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '452787406861370');
+              fbq('track', 'PageView');
+              <!-- End Meta Pixel Code -->
+          `}
+          </Script>
+          <noscript>
+            {/* eslint-disable-next-line @next/next/no-img-element,jsx-a11y/alt-text */}
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=452787406861370&ev=PageView&noscript=1"
+            />
+          </noscript>
         </Head>
         <body>
           <Main />
